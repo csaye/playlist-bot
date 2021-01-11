b3f8fc6200c80202e4a352e98086ca772d439225
Cooper Saye <coopersaye@gmail.com>
2021-01-11 08:47:19 -0800

    Implement genre list and year range.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
 import Spotify from './Spotify';
+import { genres } from './info/Genres';
 
-const genres = [
-  'any',
-  'rock',
-  'chill',
-  'folk'
-];
+const minimumYear = 1960;
 
 function App() {
   return (
@@ -22,12 +18,19 @@ function App() {
 function Page() {
   let [tracks, setTracks] = useState([]);
   let [title, setTitle] = useState('');
-  let [genre, setGenre] = useState(genres[0])
+  let [genre, setGenre] = useState(genres[0]);
+  let [minYear, setMinYear] = useState(minimumYear);
+  let [maxYear, setMaxYear] = useState(currentYear());
+
+  function currentYear() {
+    let d = new Date();
+    return d.getFullYear();
+  }
 
   async function search(e) {
     e.preventDefault();
     let g = genre === 'any' ? '' : genre;
-    Spotify.search(title, g, 10, '1000-3000').then(result => {
+    Spotify.search(title, g, 10, `${minYear}-${maxYear}`).then(result => {
       if (result.length === 0) alert('No tracks found');
       setTracks(result);
     });
@@ -41,10 +44,27 @@ function Page() {
     alert('Playlist saved successfully');
   }
 
+  function setMinYearLabel(e) {
+    setMinYear(e.target.value);
+    let l = document.getElementById('minYearLabel');
+    if (l) {
+      l.innerHTML = e.target.value;
+    }
+  }
+
+  function setMaxYearLabel(e) {
+    setMaxYear(e.target.value);
+    let l = document.getElementById('maxYearLabel');
+    if (l) {
+      l.innerHTML = e.target.value;
+    }
+  }
+
   return (
     <div>
       <form onSubmit={search}>
         <input value={title} placeholder="Title" maxLength="64" onChange={(e) => setTitle(e.target.value)} />
+
         <select value={genre} onChange={(e) => setGenre(e.target.value)}>
           {
             genres.map(g => (
@@ -52,6 +72,27 @@ function Page() {
             ))
           }
         </select>
+
+        <p id="minYearLabel">{minYear}</p>
+        <input
+        id="range-minyear"
+        type="range"
+        min={minimumYear} max={currentYear()} defaultValue={minYear}
+        step="1"
+        onChange={setMinYearLabel}
+        / >
+        <label htmlFor="range-minyear">Min Year</label>
+
+        <p id="maxYearLabel">{currentYear()}</p>
+        <input
+        id="range-maxyear"
+        type="range"
+        min={minimumYear} max={currentYear()} defaultValue={currentYear()}
+        step="1"
+        onChange={setMaxYearLabel}
+        / >
+        <label htmlFor="range-maxyear">Max Year</label>
+
         <button type="submit">Search</button>
       </form>
       <div>
