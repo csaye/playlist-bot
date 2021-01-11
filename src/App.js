952e03f693cab7b80fb9bc69f582d2a1f60296bc
Cooper Saye <coopersaye@gmail.com>
2021-01-11 14:17:30 -0800

    Clean up styling.

--- src/App.js
+++ src/App.js
@@ -34,6 +34,8 @@ function Page() {
   let [limit, setLimit] = useState(15);
   let [playlistName, setPlaylistName] = useState('');
 
+  let [loading, setLoading] = useState(false);
+
   function currentYear() {
     let d = new Date();
     return d.getFullYear();
@@ -41,12 +43,17 @@ function Page() {
 
   async function search(e) {
     e.preventDefault();
+    setLoading(true);
+    setPlaylistName('');
+
     let g = genre === 'any genre' ? '' : genre;
     let years = `${minYear}-${maxYear}`;
     let offset = randomRange(0, maxOffset);
     let notT = notTerms ? ` NOT ${notTerms}` : '';
+
     Spotify.search(terms, notT, g, limit, years, offset).then(result => {
       if (result.length === 0) alert('No tracks found');
+      setLoading(false);
       setTracks(result);
     });
   }
@@ -63,68 +70,70 @@ function Page() {
   function setMinYearLabel(e) {
     setMinYear(e.target.value);
     let l = document.getElementById('minYearLabel');
-    if (l) { l.innerHTML = e.target.value; }
+    if (l) { l.innerHTML = "Min Year: " + e.target.value; }
   }
   function setMaxYearLabel(e) {
     setMaxYear(e.target.value);
     let l = document.getElementById('maxYearLabel');
-    if (l) { l.innerHTML = e.target.value; }
+    if (l) { l.innerHTML = "Max Year: " + e.target.value; }
   }
   function setLimitLabel(e) {
     setLimit(e.target.value);
     let l = document.getElementById('limitLabel');
-    if (l) { l.innerHTML = e.target.value; }
+    if (l) { l.innerHTML = "Track Count: " + e.target.value; }
   }
 
   return (
     <div className="Page">
-      <form onSubmit={search}>
-        <input value={terms} placeholder="keyword(s)" maxLength="64" onChange={(e) => setTerms(e.target.value)} />
-        <input value={notTerms} placeholder="excluded keyword(s)" maxLength="64" onChange={(e) => setNotTerms(e.target.value)} />
+      <div className="sidebar">
+        <form onSubmit={search}>
+          <input className="textInput firstElem" value={terms} placeholder="keyword(s)" maxLength="64" onChange={(e) => setTerms(e.target.value)} />
+          <input className="textInput" value={notTerms} placeholder="excluded keyword(s)" maxLength="64" onChange={(e) => setNotTerms(e.target.value)} />
 
-        <select value={genre} onChange={(e) => setGenre(e.target.value)}>
-          {
-            genres.map(g => (
-              <option key={`genre-${g}`} value={g}>{g}</option>
-            ))
-          }
-        </select>
+          <select value={genre} onChange={(e) => setGenre(e.target.value)}>
+            {
+              genres.map(g => (
+                <option key={`genre-${g}`} value={g}>{g}</option>
+              ))
+            }
+          </select>
 
-        <p id="minYearLabel">{minYear}</p>
-        <input
-        id="range-minyear"
-        type="range"
-        min={minimumYear} max={currentYear()} defaultValue={minYear}
-        step="1"
-        onChange={setMinYearLabel}
-        / >
-        <label htmlFor="range-minyear">Min Year</label>
+          <label htmlFor="range-minyear" id="minYearLabel">Min Year: {minYear}</label>
+          <input
+          id="range-minyear"
+          type="range"
+          min={minimumYear} max={currentYear()} defaultValue={minYear}
+          step="1"
+          onChange={setMinYearLabel}
+          / >
 
-        <p id="maxYearLabel">{currentYear()}</p>
-        <input
-        id="range-maxyear"
-        type="range"
-        min={minimumYear} max={currentYear()} defaultValue={currentYear()}
-        step="1"
-        onChange={setMaxYearLabel}
-        / >
-        <label htmlFor="range-maxyear">Max Year</label>
+          <label htmlFor="range-maxyear" id="maxYearLabel">Max Year: {currentYear()}</label>
+          <input
+          id="range-maxyear"
+          type="range"
+          min={minimumYear} max={currentYear()} defaultValue={currentYear()}
+          step="1"
+          onChange={setMaxYearLabel}
+          / >
 
-        <p id="limitLabel">{limit}</p>
-        <input
-        id="range-limit"
-        type="range"
-        min="1" max="30" defaultValue="15"
-        step="1"
-        onChange={setLimitLabel}
-        / >
-        <label htmlFor="range-limit">Track Count</label>
+          <label htmlFor="range-limit" id="limitLabel">Track Count: 15</label>
+          <input
+          id="range-limit"
+          type="range"
+          min="1" max="30" defaultValue="15"
+          step="1"
+          onChange={setLimitLabel}
+          / >
 
-        <button type="submit">Generate Playlist</button>
-      </form>
-      <div>
-        { tracks.length > 0 &&
-          <div>
+          <button type="submit">Generate Playlist</button>
+        </form>
+      </div>
+      <div className="listing">
+        {
+          loading && <p className="loadingText" id="loadingText">Loading...</p>
+        }
+        { (tracks.length > 0 && !loading) &&
+          <div className="saveTrack">
             <input
             value={playlistName}
             placeholder="playlist name"
@@ -135,7 +144,7 @@ function Page() {
           </div>
         }
         {
-          tracks.map(t => <Track key={t.id} message={t} />)
+          !loading && tracks.map(t => <Track key={t.id} message={t} />)
         }
       </div>
     </div>
@@ -149,7 +158,11 @@ function Track(props) {
     <div className="Track">
       <a href={url}>
         <img src={image} alt={album} />
-        <p>{name} <b>|</b> {artist} | {album}</p>
+        <div className="text">
+          <h1>{name}</h1>
+          <p>{artist}</p>
+          <p>{album}</p>
+        </div>
       </a>
     </div>
   );
