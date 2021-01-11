f8c5331f9939bb6549fbd1b1e2634eb048b2e9a3
Cooper Saye <coopersaye@gmail.com>
2021-01-10 22:44:33 -0800

    Implement genres and playlist saving.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 import Spotify from './Spotify';
 
+const genres = [
+  'any',
+  'rock',
+  'chill',
+  'folk'
+];
+
 function App() {
   return (
     <div className="App">
@@ -15,28 +22,48 @@ function App() {
 function Page() {
   let [tracks, setTracks] = useState([]);
   let [title, setTitle] = useState('');
+  let [genre, setGenre] = useState(genres[0])
 
-  const search = async(e) => {
+  async function search(e) {
     e.preventDefault();
-    Spotify.search(title, 'chill', 10, '1000-3000').then(result => {
-      if (result.length == 0) window.alert('No tracks found');
+    let g = genre === 'any' ? '' : genre;
+    Spotify.search(title, g, 10, '1000-3000').then(result => {
+      if (result.length === 0) alert('No tracks found');
       setTracks(result);
     });
+  }
+
+  function savePlaylist() {
+    let trackURIs = tracks.map(t => {
+      return t.uri;
+    });
+    Spotify.savePlaylist('Bot Playlist', trackURIs);
+    alert('Playlist saved successfully');
   }
 
   return (
     <div>
       <form onSubmit={search}>
-        <input value={title} placeholder="Title" maxLength="64" onChange={(e) => setTitle(e.target.value)} required />
+        <input value={title} placeholder="Title" maxLength="64" onChange={(e) => setTitle(e.target.value)} />
+        <select value={genre} onChange={(e) => setGenre(e.target.value)}>
+          {
+            genres.map(g => (
+              <option key={`genre-${g}`} value={g}>{g}</option>
+            ))
+          }
+        </select>
         <button type="submit">Search</button>
       </form>
-      {
-        tracks?.map(t => (
-          <div key={t.id}>
-            <p>{t.name} | {t.artist} | {t.album}</p>
-          </div>
-        ))
-      }
+      <div>
+        {
+          tracks.map(t => (
+            <div key={t.id}>
+              <p>{t.name} | {t.artist} | {t.album}</p>
+            </div>
+          ))
+        }
+        { tracks.length > 0 && <button onClick={savePlaylist}>Save Playlist</button> }
+      </div>
     </div>
   );
 }
